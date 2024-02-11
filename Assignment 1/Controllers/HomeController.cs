912353af39dab0d9d37725c1e3989793aff57815
Pritom Chandra Dey <133569468+Pritomkk@users.noreply.github.com>
2024-02-11 15:45:16 +0600

    Assignment 1

---
+++ Assignment 1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+
+
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            
+
+            return View();
+        }
+
+        public ActionResult Ref()
+        {
+
+
+            return View();
+        }
+
+        public ActionResult P_info()
+        {
+
+
+            return View();
+        }
+    }
+}
